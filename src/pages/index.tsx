02797fd7f83be947ef16535b91f153ff0e2b8986
fabianoobispo <fbc623@gmail.com>
2021-03-30 17:28:53 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
 
+import { FiUser, FiCalendar } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -25,8 +28,33 @@ interface HomeProps {
 }
 
 export default function Home() {
-  return <h1>test</h1>;
-  // TODO
+  return (
+    <>
+      <head>
+        <title>Home | spacetraveling</title>
+      </head>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.content}>
+          <a>
+            <h1>titulo</h1>
+            <h3>sub titulo</h3>
+            <div className={styles.postFooter}>
+              <time>
+                <FiCalendar />
+                data{' '}
+              </time>
+              <p>
+                <FiUser />
+                autor{' '}
+              </p>
+            </div>
+          </a>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
 }
 
 // export const getStaticProps = async () => {
